52e0feff4852c808432f585704f9ae97105ad828
이윤선 <yslee@colosseum.kr>
2025-09-23 11:56:54 +0900

    github package && gradle version catalogs

--- skeleton-library-excel/build.gradle.kts
+++ skeleton-library-excel/build.gradle.kts
@@ -2,9 +2,35 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 description = "skeleton-library-excel"
 
+plugins {
+	`maven-publish`
+	`java-library`
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("gpr") {
+			from(components["java"]) // 어떤 빌드 결과물을 아티팩트로 삼을지 지정
+			groupId = "kr.skeleton"
+			artifactId = "skeleton-library-excel"
+			version = "0.0.2"
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/yoonseonlee-colosseum/todo-library")
+			credentials {
+				username = rootProject.findProperty("gpr.user") as String
+				password = rootProject.findProperty("gpr.key") as String
+			}
+		}
+	}
+}
+
 dependencies {
 	implementation(project(":skeleton-library-shared"))
-	implementation("org.apache.poi:poi-ooxml:5.4.1")
+	implementation(libs.poi.ooxml)
 }
 
 tasks.withType<Jar> {
